5ae881844a229f66a0a6a45aa020b9d559e80531
aditya-K2 <adityakurdunkar2@gmail.com>
2021-12-12 17:41:14 +0530

    responsive design

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ function toggleDarkMode(){
 	document.getElementsByTagName("i")[0].classList.toggle("fa-sun");
 	document.getElementsByTagName("i")[0].classList.toggle("fa-moon");
 	if ( !lightMode ){
-		document.getElementsByClassName("github")[0].setAttribute("src", "./assets/githubD.png")
-		document.getElementsByClassName("twitter")[0].setAttribute("src", "./assets/twitterD.png")
-		document.getElementsByClassName("linkedin")[0].setAttribute("src", "./assets/linkedinD.png")
+		document.getElementsByClassName("github")[0].setAttribute("src", "./assets/githubD.svg")
+		document.getElementsByClassName("twitter")[0].setAttribute("src", "./assets/twitterD.svg")
+		document.getElementsByClassName("linkedin")[0].setAttribute("src", "./assets/linkedinD.svg")
 		lightMode = true
 	} else {
-		document.getElementsByClassName("github")[0].setAttribute("src", "./assets/github.png")
+		document.getElementsByClassName("github")[0].setAttribute("src", "./assets/github.svg")
 		document.getElementsByClassName("twitter")[0].setAttribute("src", "./assets/twitter.png")
-		document.getElementsByClassName("linkedin")[0].setAttribute("src", "./assets/linkedin.png")
+		document.getElementsByClassName("linkedin")[0].setAttribute("src", "./assets/linkedin.svg")
 		lightMode = false
 	}
 }
