29effbce3e04bbfe7fa1ab1e5cd622d665744770
aditya-K2 <adityakurdunkar2@gmail.com>
2021-12-13 19:53:49 +0530

    default theme is now light

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-let lightMode = false;
+let darkMode = false;
 
 function toggleDarkMode(){
-	document.body.classList.toggle("lightMode")
+	document.body.classList.toggle("darkMode")
 	document.getElementsByTagName("i")[0].classList.toggle("fa-sun");
 	document.getElementsByTagName("i")[0].classList.toggle("fa-moon");
-	if ( !lightMode ){
-		document.getElementsByClassName("github")[0].setAttribute("src", "./assets/githubD.svg")
-		document.getElementsByClassName("twitter")[0].setAttribute("src", "./assets/twitterD.svg")
-		document.getElementsByClassName("linkedin")[0].setAttribute("src", "./assets/linkedinD.svg")
-		lightMode = true
-	} else {
+	if ( !darkMode ){
 		document.getElementsByClassName("github")[0].setAttribute("src", "./assets/github.svg")
 		document.getElementsByClassName("twitter")[0].setAttribute("src", "./assets/twitter.svg")
 		document.getElementsByClassName("linkedin")[0].setAttribute("src", "./assets/linkedin.svg")
-		lightMode = false
+		darkMode = true
+	} else {
+		document.getElementsByClassName("github")[0].setAttribute("src", "./assets/githubD.svg")
+		document.getElementsByClassName("twitter")[0].setAttribute("src", "./assets/twitterD.svg")
+		document.getElementsByClassName("linkedin")[0].setAttribute("src", "./assets/linkedinD.svg")
+		darkMode = false
 	}
 }
